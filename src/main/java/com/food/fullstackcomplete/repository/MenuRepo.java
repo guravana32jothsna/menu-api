1b9e8dd37e20308dabc8a03df858b56936220cb4
guravana32jothsna <152513180+guravana32jothsna@users.noreply.github.com>
2023-12-18 10:56:49 +0530

    Delete src directory

--- src/main/java/com/food/fullstackcomplete/repository/MenuRepo.java
+++
@@ -1,9 +0,0 @@
-package com.food.fullstackcomplete.repository;
-
-import com.food.fullstackcomplete.model.Menu_db;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepo extends JpaRepository <Menu_db , Integer>{
-}
